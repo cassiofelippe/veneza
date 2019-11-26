02963669d12b1c10500b2e8ea9db91e44fce62b2
Cassio <cassioluis12@gmail.com>
2019-11-25 21:06:11 -0300

    initial commit, added spring stuff

---
+++ src/main/java/br/com/cassio/veneza/VenezaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cassio.veneza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cassio/veneza/VenezaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cassio.veneza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
