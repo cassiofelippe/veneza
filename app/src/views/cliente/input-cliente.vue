0da7323dfca655058131c021acb802d2faa87a87
Cassio <cassioluis12@gmail.com>
2019-12-16 18:17:14 -0300

    Added frontend validations

--- app/src/views/cliente/input-cliente.vue
+++ app/src/views/cliente/input-cliente.vue
@@ -5,17 +5,17 @@
     <b-row>
       <b-col cols="12" sm="12" md="12" lg="6">
         <label>Nome</label>
-        <b-form-input id="cliente-nome" :disabled="disabled" v-model="model.nome" />
+        <b-form-input id="cliente-nome" :disabled="disabled" v-model="model.nome" required />
       </b-col>
       
       <b-col cols="12" sm="12" md="6" lg="3">
         <label>CPF</label>
-        <the-mask id="cliente-cpf" :disabled=disabled pattern="\d*" v-model="model.cpf" :mask="'###.###.###-##'" placeholder="000.000.000-00" class="form-control" />
+        <the-mask id="cliente-cpf" :disabled=disabled pattern="\d*" v-model="model.cpf" :mask="'###.###.###-##'" placeholder="000.000.000-00" class="form-control" required />
       </b-col>
 
       <b-col cols="12" sm="12" md="6" lg="3">
         <label>Telefone</label>
-        <the-mask id="cliente-telefone" :disabled="disabled" pattern="\d*" v-model="model.telefone" :mask="['(##) ####-####', '(##) #####-####']" placeholder="(00) 00000-0000" class="form-control" />
+        <the-mask id="cliente-telefone" :disabled="disabled" pattern="\d*" v-model="model.telefone" :mask="['(##) ####-####', '(##) #####-####']" placeholder="(00) 00000-0000" class="form-control" required />
       </b-col>
 
       <b-col cols="12" sm="12" md="12" lg="12">&nbsp;</b-col>
@@ -51,12 +51,16 @@
 
     methods: {
       save() {
-        http.post('clientes', this.model).then((response) => {
-          this.model = response;
-          this.$router.push({name: 'clientes-show', params: {_id: this.model._id}}).catch(e => e).then(() => {
-            this.$router.go();
+        if (!this.model.nome || !this.model.cpf || !this.model.telefone) {
+          alert('Deve preencher todos os campos!');
+        } else {
+          http.post('clientes', this.model).then((response) => {
+            this.model = response;
+            this.$router.push({name: 'clientes-show', params: {_id: this.model._id}}).catch(e => e).then(() => {
+              this.$router.go();
+            });
           });
-        });
+        }
       },
 
       show(_id) {
